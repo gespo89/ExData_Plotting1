ccf572b00248d3c9554d9df339e7800a7acd4f9d
Geoffrey Esposito <gespo89@gmail.com>
2015-06-06 23:45:13 -0400

    Change some function calls to their equivalent but more appropriate counterparts

--- plot3.R
+++ plot3.R
@@ -12,8 +12,8 @@ data$timestamp <- (strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:
 makeplot <- function(){
   #Make the plot
   plot(data$timestamp, data$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
-  points(data$timestamp, data$Sub_metering_2, type="l", col="red")
-  points(data$timestamp, data$Sub_metering_3, type="l", col="blue")
+  lines(data$timestamp, data$Sub_metering_2, col="red")
+  lines(data$timestamp, data$Sub_metering_3, col="blue")
   legend("topright",  col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1))
 }
 
--- plot4.R
+++ plot4.R
@@ -28,8 +28,8 @@ makeplot <- function(){
   
   # Make the bottom left plot
   plot(data$timestamp, data$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
-  points(data$timestamp, data$Sub_metering_2, type="l", col="red")
-  points(data$timestamp, data$Sub_metering_3, type="l", col="blue")
+  lines(data$timestamp, data$Sub_metering_2, col="red")
+  lines(data$timestamp, data$Sub_metering_3, col="blue")
   legend("topright",  col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1))
   
   # Make the bottom right plot
